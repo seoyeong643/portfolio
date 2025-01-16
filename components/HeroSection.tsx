1230399e5a473b65d4ebe72083d66975ca959775
Maia Oh <seoyeong.oh364@gmail.com>
2025-01-15 21:08:36 -0700

    Add Githb, Linkedin, Gmail buttons

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Link } from "react-scroll/modules"
 import { HiArrowDown } from "react-icons/hi"
 import { useTheme } from "next-themes";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const HeroSection = () => {
   const { systemTheme, theme } = useTheme();
@@ -46,17 +47,46 @@ const HeroSection = () => {
             based in Arizona. Working towards creating software that
             makes life easier and more meaningful.
           </p>
-          <Link
-            to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Projects
-          </Link>
+
+          <div className="flex space-x-4 mt-6">
+            <a
+              href="https://github.com/seoyeong643"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gray-700 hover:bg-gray-800 text-white rounded-full p-2 flex items-center justify-center shadow-md"
+              style={{ width: "40px", height: "40px" }}
+            >
+              <FaGithub size={24} />
+            </a>
+            <a
+              href="https://linkedin.com/in/seoyeongoh"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-2 flex items-center justify-center shadow-md"
+              style={{ width: "40px", height: "40px" }}
+            >
+              <FaLinkedin size={24} />
+            </a>
+            <a
+              href="mailto:seoyeong.oh364@gmail.com"
+              className="bg-red-500 hover:bg-red-600 text-white rounded-full p-2 flex items-center justify-center shadow-md"
+              style={{ width: "40px", height: "40px" }}
+            >
+              <FaEnvelope size={24} />
+            </a>
+            {/* <Link
+              to="projects"
+              className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              Projects
+            </Link> */}
+          </div>
+
         </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
