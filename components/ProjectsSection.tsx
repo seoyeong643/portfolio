48fb68a560b7344a5a9fb8f3b419580b1aa63ee2
Maia Oh <seoyeong.oh364@gmail.com>
2025-02-05 22:34:25 -0700

    Refactor: move projects data to separate file in data folder

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -3,34 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import SlideUp from "./SlideUp";
 import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
-
-const projects = [
-  {
-    name: "Intel: Data Analysis for Sustainability Project",
-    description:
-      "Developed SQL queries to investigate problems and make data-driven recommendations to support Intel team decisions for the new data center location. -Tech: SQL, Tableau",
-    image: "/intel.png",
-    github: "https://github.com/seoyeong643",
-    link: "https://docs.google.com/document/d/1rdO3fg9JPfzDVPKcsTu_lKHh7YRVzSzQJMICQzoc7sI/edit?usp=sharing",
-  },
-  {
-    name: "Grammy Awards: Website Audience Analysis",
-    description:
-      "Analysis and visualization of performance metrics for the Grammy Awards website in relation to content and audience strategy. -Tech: Python, Pandas, Numpy, Plotly",
-    image: "/grammy.png",
-    github:
-      "https://github.com/seoyeong643/Grammy-Awards-Website-Audience-Analysis",
-    link: "https://github.com/seoyeong643/Grammy-Awards-Website-Audience-Analysis",
-  },
-  {
-    name: "Drive Insight Website",
-    description:
-      "Product dashboard website that effectively integrates vehicle compatibility features using a RESTful API, ensuring users have immediate access to essential information. - Tech: NextJS, React, TypeScript, Tailwind",
-    image: "/driveInsight.png",
-    github: "https://github.com/seoyeong643/drive-insight-website",
-    link: "https://github.com/seoyeong643/drive-insight-website",
-  },
-];
+import projects from "./data/ProjectData";
 
 const ProjectsSection = () => {
   return (
---
+++ components/data/ProjectData.tsx
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    name: "Intel: Data Analysis for Sustainability Project",
+    description:
+      "Developed SQL queries to investigate problems and make data-driven recommendations to support Intel team decisions for the new data center location. -Tech: SQL, Tableau",
+    image: "/intel.png",
+    github: "https://github.com/seoyeong643",
+    link: "https://docs.google.com/document/d/1rdO3fg9JPfzDVPKcsTu_lKHh7YRVzSzQJMICQzoc7sI/edit?usp=sharing",
+  },
+  {
+    name: "Grammy Awards: Website Audience Analysis",
+    description:
+      "Analysis and visualization of performance metrics for the Grammy Awards website in relation to content and audience strategy. -Tech: Python, Pandas, Numpy, Plotly",
+    image: "/grammy.png",
+    github:
+      "https://github.com/seoyeong643/Grammy-Awards-Website-Audience-Analysis",
+    link: "https://github.com/seoyeong643/Grammy-Awards-Website-Audience-Analysis",
+  },
+  {
+    name: "Drive Insight Website",
+    description:
+      "Product dashboard website that effectively integrates vehicle compatibility features using a RESTful API, ensuring users have immediate access to essential information. - Tech: NextJS, React, TypeScript, Tailwind",
+    image: "/driveInsight.png",
+    github: "https://github.com/seoyeong643/drive-insight-website",
+    link: "https://github.com/seoyeong643/drive-insight-website",
+  },
+];
+
+export default projects;
