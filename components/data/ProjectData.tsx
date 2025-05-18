026075d6957e6833de4fac9622702e913856db6b
Maia Oh <seoyeong.oh364@gmail.com>
2025-05-17 22:06:39 -0700

    Reverse order so recent projects appears at the top

--- components/data/ProjectData.tsx
+++ components/data/ProjectData.tsx
@@ -1,11 +1,51 @@
 const projects = [
   {
-    name: "Intel: Data Analysis for Sustainability Project",
+    name: "AuraMind Mental Health Chatbot",
     description:
-      "Developed SQL queries to investigate problems and make data-driven recommendations to support Intel team decisions for the new data center location. -Tech: SQL, Tableau",
-    image: "/intel.png",
-    github: "https://github.com/seoyeong643",
-    link: "https://docs.google.com/document/d/1rdO3fg9JPfzDVPKcsTu_lKHh7YRVzSzQJMICQzoc7sI/edit?usp=sharing",
+      "Designed and developed an AI-driven chatbot focused on providing mental health support through empathetic, real-time conversations. - Tech: Gemini Pro, GenAI(LLM), LangChain, Chroma, Python",
+    image: "/AuraMind.png",
+    github: "https://kaggle.com/code/shreysharma07/auramind",
+    link: "https://kaggle.com/code/shreysharma07/auramind",
+  },
+  {
+    name: "AI-powered Diabetes Prediction Web App - AI Hackathon",
+    description:
+      "Built and trained a classification model leveraging Scikit-learn and Pandas to analyze medical data and generate predictions - Tech: Python, Pandas, Scikit-learn, Flask, HTML, CSS",
+    image: "/StepByStepWellness.png",
+    github: "https://github.com/seoyeong643/StepByStepWellness",
+    link: "https://github.com/seoyeong643/StepByStepWellness",
+  },
+  {
+    name: "Portfolio Website",
+    description:
+      "Developed and deployed a personal portfolio website to showcase my skills and experience as a web developer. - Tech: React, Next.js, TypeScript, JavaScript, Tailwind CSS, SCSS, Vercel",
+    image: "/portfolio.png",
+    github: "https://github.com/seoyeong643/portfolio",
+    link: "https://github.com/seoyeong643/portfolio",
+  },
+  {
+    name: "AI Evolution Data Visualization",
+    description:
+      "Developed an interactive data visualization project that explores key trends in AI advancement, industry influence, and global policy changes. - Tech: React, JavaScript, HTML, CSS, D3.js",
+    image: "/AIEvolution.png",
+    github: "https://github.com/seoyeong643/Rise-of-AI",
+    link: "https://github.com/seoyeong643/Rise-of-AI",
+  },
+  {
+    name: "Drive Insight Website",
+    description:
+      "Product dashboard website that effectively integrates vehicle compatibility features using a RESTful API, ensuring users have immediate access to essential information. - Tech: NextJS, React, TypeScript, Tailwind",
+    image: "/driveInsight.png",
+    github: "https://github.com/seoyeong643/drive-insight-website",
+    link: "https://github.com/seoyeong643/drive-insight-website",
+  },
+  {
+    name: "Bookstore",
+    description:
+      "A simple Spring Boot REST API for managing books. Includes endpoints to create, read, update, and delete books. -Tech: Java, Spring Boot, Relational Database",
+    image: "/bookstore1.png",
+    github: "https://github.com/seoyeong643/Bookstore/tree/main",
+    link: "https://github.com/seoyeong643/Bookstore/tree/main",
   },
   {
     name: "Grammy Awards: Website Audience Analysis",
@@ -17,52 +57,12 @@ const projects = [
     link: "https://github.com/seoyeong643/Grammy-Awards-Website-Audience-Analysis",
   },
   {
-    name: "Bookstore",
+    name: "Intel: Data Analysis for Sustainability Project",
     description:
-      "A simple Spring Boot REST API for managing books. Includes endpoints to create, read, update, and delete books. -Tech: Java, Spring Boot, Relational Database",
-    image: "/bookstore1.png",
-    github: "https://github.com/seoyeong643/Bookstore/tree/main",
-    link: "https://github.com/seoyeong643/Bookstore/tree/main",
-  },
-  {
-    name: "Drive Insight Website",
-    description:
-      "Product dashboard website that effectively integrates vehicle compatibility features using a RESTful API, ensuring users have immediate access to essential information. - Tech: NextJS, React, TypeScript, Tailwind",
-    image: "/driveInsight.png",
-    github: "https://github.com/seoyeong643/drive-insight-website",
-    link: "https://github.com/seoyeong643/drive-insight-website",
-  },
-  {
-    name: "AI Evolution Data Visualization",
-    description:
-      "Developed an interactive data visualization project that explores key trends in AI advancement, industry influence, and global policy changes. - Tech: React, JavaScript, HTML, CSS, D3.js",
-    image: "/AIEvolution.png",
-    github: "https://github.com/seoyeong643/Rise-of-AI",
-    link: "https://github.com/seoyeong643/Rise-of-AI",
-  },
-  {
-    name: "Portfolio Website",
-    description:
-      "Developed and deployed a personal portfolio website to showcase my skills and experience as a web developer. - Tech: React, Next.js, TypeScript, JavaScript, Tailwind CSS, SCSS, Vercel",
-    image: "/portfolio.png",
-    github: "https://github.com/seoyeong643/portfolio",
-    link: "https://github.com/seoyeong643/portfolio",
-  },
-  {
-    name: "AI-powered Diabetes Prediction Web App - AI Hackathon",
-    description:
-      "Built and trained a classification model leveraging Scikit-learn and Pandas to analyze medical data and generate predictions - Tech: Python, Pandas, Scikit-learn, Flask, HTML, CSS",
-    image: "/StepByStepWellness.png",
-    github: "https://github.com/seoyeong643/StepByStepWellness",
-    link: "https://github.com/seoyeong643/StepByStepWellness",
-  },
-  {
-    name: "AuraMind Mental Health Chatbot",
-    description:
-      "Designed and developed an AI-driven chatbot focused on providing mental health support through empathetic, real-time conversations. - Tech: Gemini Pro, GenAI(LLM), LangChain, Chroma, Python",
-    image: "/AuraMind.png",
-    github: "https://kaggle.com/code/shreysharma07/auramind",
-    link: "https://kaggle.com/code/shreysharma07/auramind",
+      "Developed SQL queries to investigate problems and make data-driven recommendations to support Intel team decisions for the new data center location. -Tech: SQL, Tableau",
+    image: "/intel.png",
+    github: "https://github.com/seoyeong643",
+    link: "https://docs.google.com/document/d/1rdO3fg9JPfzDVPKcsTu_lKHh7YRVzSzQJMICQzoc7sI/edit?usp=sharing",
   },
 ];
 
