6c392498383d33c2ac0ae2bfa70a219368c3a0b1
Maia Oh <seoyeong.oh364@gmail.com>
2025-10-15 23:45:11 -0700

    Style: update projects layout from single column to two-column layout

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -13,24 +13,22 @@ const ProjectsSection = () => {
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
       </h1>
 
-      <div className="flex flex-col space-y-28">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
         {projects.map((project, idx) => {
           return (
             <div key={idx}>
               <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
-                  <div className=" md:w-1/2">
-                    <Link href={project.link}>
-                      <Image
-                        src={project.image}
-                        alt=""
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
-                      />
-                    </Link>
-                  </div>
-                  <div className="mt-8 md:w-1/2">
+                <div className="flex flex-col items-center animate-slideUpCubiBezier animation-delay-2 space-y-6">
+                  <Link href={project.link}>
+                    <Image
+                      src={project.image}
+                      alt=""
+                      width={1000}
+                      height={1000}
+                      className="rounded-xl shadow-xl hover:opacity-70"
+                    />
+                  </Link>
+                  <div>
                     <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
                     <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
                       {project.description}
